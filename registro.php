9cd71b97242c52d40d5ac7b61310b7d13813f2a0
Nombre del estudiante <correo@ejemplo.com>
2026-06-04 21:44:15 +0700

    cambio php

--- registro.php
+++ registro.php
@@ -1,24 +1,25 @@
 <?php
 
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$telefono = $_POST['telefono'];
-$direccion = $_POST['direccion'];
-$password = $_POST['pasword'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-echo "<h1>Registro Exitoso</h1>";
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $direccion = $_POST['direccion'];
+    $password = $_POST['password'];
 
-<?php
-header("Location: dashboard.html");
-exit();
-?>
+    echo "<h1>Registro Exitoso</h1>";
 
-echo "<p><strong>Nombre:</strong> $nombre</p>";
-echo "<p><strong>Correo:</strong> $correo</p>";
-echo "<p><strong>Teléfono:</strong> $telefono</p>";
-echo "<p><strong>Dirección:</strong> $direccion</p>";
+    echo "<p><strong>Nombre:</strong> $nombre</p>";
+    echo "<p><strong>Correo:</strong> $correo</p>";
+    echo "<p><strong>Teléfono:</strong> $telefono</p>";
+    echo "<p><strong>Dirección:</strong> $direccion</p>";
 
-echo "<br>";
-echo "<a href='index.html'>Volver al Inicio</a>";
+    echo "<br>";
+    echo "<a href='index.html'>Volver al Inicio</a>";
+
+} else {
+    echo "Acceso no válido";
+}
 
 ?>
